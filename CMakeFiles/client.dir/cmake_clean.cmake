c1a590fa09c446688622370fb9e00f4e98a263a7
Sebastian Ripari <sebastiandripari@gmail.com>
2017-10-18 16:16:27 -0300

    Move make place

---
+++ CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/src/client/StartWindow.cpp.o"
+  "CMakeFiles/client.dir/src/client/main.cpp.o"
+  "CMakeFiles/client.dir/src/common/Thread.cpp.o"
+  "CMakeFiles/client.dir/src/common/Socket.cpp.o"
+  "client.pdb"
+  "client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
