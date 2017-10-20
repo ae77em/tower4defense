932b4304406fc2e94109f65952bcec3b812a5e2d
Pablo Repetto <caussabon@gmail.com>
2017-10-20 00:08:29 -0300

    Arreglo el codigo de ejemplo

--- tests/dummy_test.cpp
+++ tests/dummy_test.cpp
@@ -1,7 +1,7 @@
 #include "catch.h"
 
 unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+    return number > 1 ? Factorial(number-1)*number : 1;
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
