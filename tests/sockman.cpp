f890a7aea53420e93dc51d612e4b503f4cf85bb9
Pablo Ernesto Repetto <caussabon@gmail.com>
2017-10-24 00:20:08 -0300

    Agrego argumento opcional puerto a prueba

--- tests/sockman.cpp
+++ tests/sockman.cpp
@@ -1,9 +1,12 @@
 #include "SocketManager.h"
 #include "Socket.h"
+#include <cstdlib>
 
 int main(int argc, char **argv) {
+    uint16_t port = argv[1] ? atoi(argv[1]) : 8080;
+
     Socket acceptor;
-    acceptor.bind(8080);
+    acceptor.bind(port);
     acceptor.listen();
 
     SocketManager server(Socket(acceptor.accept()));
