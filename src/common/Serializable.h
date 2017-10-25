c4f0d8f10f76878181f5102d3bdff6e178aa6664
Pepo <aeisner@grayshirts.com.ar>
2017-10-24 23:00:39 -0300

    REsuelvo conflictos.

---
+++ src/common/Serializable.h
@@ -0,0 +1,16 @@
+#ifndef TP4_TOWERDEFENSE_SERIALIZABLE_H
+#define TP4_TOWERDEFENSE_SERIALIZABLE_H
+
+#include <string>
+
+/* "Interface" (kind of) to implement a serializable class. */
+class Serializable {
+	public:
+		Serializable();
+		virtual ~Serializable();
+
+		virtual std::string serialize() = 0;
+		virtual bool deserialize(std::string obj_serial) = 0;
+};
+
+#endif
