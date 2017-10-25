23bbcc13d72eadbc45a9ab75acde2b3d7abe722d
Pablo Repetto <caussabon@gmail.com>
2017-10-24 23:45:19 -0300

    Creo dummy de Juego
    Incluyo solo el metodo agregar torre

---
+++ include/Juego.h
@@ -0,0 +1,16 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+#define T Juego
+
+/* -----> x
+ * |
+ * v
+ * y
+ */
+class T {
+public:
+    void agregarTorre(unsigned x, unsigned y);
+};
+
+#undef T
+#endif
---
+++ src/server/Juego.cpp
@@ -0,0 +1,7 @@
+#include "Juego.h"
+
+#define T Juego
+
+void T::agregarTorre(unsigned x, unsigned y) {
+}
+
