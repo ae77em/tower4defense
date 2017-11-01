f07cd7aa26c81274cbcf5023318169c60a79e3fe
Pablo Repetto <caussabon@gmail.com>
2017-11-01 19:03:16 -0300

    Refactor menor en main

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -1,3 +1,5 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include "../sdl/Screen.h"
 #include <stdexcept>
 #include "../common/Point.h"
@@ -26,14 +28,15 @@ int main(int argc, char *argv[]) {
         SDL_Event e;
         while(SDL_PollEvent( &e ) != 0) {
             if (e.type == SDL_QUIT) quit = true;
-            else if (e.type == SDL_MOUSEBUTTONDOWN) {
+
+            if (e.type == SDL_MOUSEBUTTONDOWN) {
                 Point p = screen.mouseCurrentTile();
                 Point d = mapa.dimensiones();
                 if ((p.isPositive()) && (p.x < d.x) && (p.y < d.y))
                     mapa.setCasilla('*', p.x, p.y);
-            } else if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP) {
-                screen.handleEvent(e);
             }
+
+            screen.handleEvent(e);
         }
 
         screen.put(mapa);
