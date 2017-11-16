0e9d2530c8013c6f0e251659ee18a003349a146e
Pablo Repetto <caussabon@gmail.com>
2017-11-16 20:15:57 -0300

    Mapa: implemento metodo estaDentro

--- src/editor/StatePath.cpp
+++ src/editor/StatePath.cpp
@@ -5,22 +5,18 @@ void Editor::StatePath::handle(const SDL_Event &e, Editor &context) {
     if (e.type == SDL_MOUSEBUTTONDOWN
             && !( KMOD_SHIFT & SDL_GetModState() )) {
         Point p = context.getScreen().mouseCurrentTile();
-        Point d = context.getMap().dimensiones();
-        if ((p.isPositive()) && (p.x < d.x) && (p.y < d.y))
-            path.push_back(p);
+        if (context.getMap().estaDentro(p)) path.push_back(p);
     }
 
     // Shift-click adds point to path, ends it, and adds it to map
     if (e.type == SDL_MOUSEBUTTONDOWN
             && ( KMOD_SHIFT & SDL_GetModState() )) {
         Point p = context.getScreen().mouseCurrentTile();
-        Point d = context.getMap().dimensiones();
 
         /* Do something iff the new point is acceptable as
            an extension of the path, and the result would be
            a sensible path */
-        if ((p.isPositive()) && (p.x < d.x) && (p.y < d.y)
-                && (path.size() > 0)) {
+        if (context.getMap().estaDentro(p) && (path.size() > 0)) {
             path.push_back(p);
             context.getMap().agregarCamino(path);
             path.clear();
